c2a712d6bd09947e20c35c3cfa3c5e880536d5a7
yassineltayeb <yassineltayeb@live.com>
2022-11-23 14:25:19 +0400

    code refactor

--- Domain/Entities/BaseUser.cs
+++ Domain/Entities/BaseUser.cs
@@ -7,13 +7,13 @@ public class BaseUser
     public long Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    [ForeignKey("Gender")]
+    [ForeignKey(nameof(Gender))]
     public int? GenderId { get; set; }
     public Gender Gender { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
     public string Password { get; set; }
-    public DateTime? DateOfBirth { get; set; }  
+    public DateTime? DateOfBirth { get; set; }
     public long CreatedAt { get; set; }
     public long UpdatedAt { get; set; }
     public ICollection<CoachMember> CoachMember { get; set; }
