d5c71cebd50070b1470d415d171a125328eb70cf
yassineltayeb <yassineltayeb@live.com>
2022-11-23 16:27:04 +0400

    added cities controller

---
+++ FitnessManagementSoftware/Controller/CitiesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Interface;
+using Service.ViewModels.Common;
+
+namespace FitnessManagementSoftware.Controller;
+
+[Route("api/cities")]
+[ApiController]
+public class CitiesController : ControllerBase
+{
+    private readonly ICityService _cityService;
+
+    public CitiesController(ICityService cityService)
+	{
+        _cityService = cityService;
+    }
+
+    [HttpGet()]
+    public async Task<List<KeyValuePairs>> GetCitiesByCountryId(int countryId)
+    {
+        return await _cityService.GetCitiesByCountryId(countryId);
+    }
+}
