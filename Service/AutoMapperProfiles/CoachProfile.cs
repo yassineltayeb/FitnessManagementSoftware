b70ec409f091fb815fff449837545a5ab5f96f58
yassineltayeb <yassineltayeb@live.com>
2022-11-23 14:41:36 +0400

    added extra columns to sign up view model

--- Service/AutoMapperProfiles/CoachProfile.cs
+++ Service/AutoMapperProfiles/CoachProfile.cs
@@ -6,15 +6,18 @@ namespace Service.AutoMapperProfiles;
 
 public class CoachProfile : Profile
 {
-	public CoachProfile()
-	{
-		CreateMap<SignUpRequestViewModel, Coach>()
-			.ForMember(desc => desc.FirstName, o => o.MapFrom(src => src.FirstName))
-			.ForMember(desc => desc.LastName, o => o.MapFrom(src => src.LastName))
-			.ForMember(desc => desc.Email, o => o.MapFrom(src => src.Email))
-			.ForMember(desc => desc.Phone, o => o.MapFrom(src => src.Phone))
-			.ForMember(desc => desc.DateOfBirth, o => o.MapFrom(src => src.DateOfBirth))
-			.ForMember(desc => desc.Password, o => o.MapFrom(src => src.Password))
-			.ReverseMap();
-	}
+    public CoachProfile()
+    {
+        CreateMap<SignUpRequestViewModel, Coach>()
+            .ForMember(desc => desc.FirstName, o => o.MapFrom(src => src.FirstName))
+            .ForMember(desc => desc.LastName, o => o.MapFrom(src => src.LastName))
+            .ForMember(desc => desc.GenderId, o => o.MapFrom(src => src.GenderId))
+            .ForMember(desc => desc.Email, o => o.MapFrom(src => src.Email))
+            .ForMember(desc => desc.Phone, o => o.MapFrom(src => src.Phone))
+            .ForMember(desc => desc.CountryId, o => o.MapFrom(src => src.CountryId))
+            .ForMember(desc => desc.CityId, o => o.MapFrom(src => src.CityId))
+            .ForMember(desc => desc.DateOfBirth, o => o.MapFrom(src => src.DateOfBirth))
+            .ForMember(desc => desc.Password, o => o.MapFrom(src => src.Password))
+            .ReverseMap();
+    }
 }
--- Service/ViewModels/Coach/SignUpRequestViewModel.cs
+++ Service/ViewModels/Coach/SignUpRequestViewModel.cs
@@ -3,8 +3,11 @@ public class SignUpRequestViewModel
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public int? GenderId { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
+    public int? CountryId { get; set; }
+    public int? CityId { get; set; }
     public DateTime? DateOfBirth { get; set; }
     public string Password { get; set; }
 }
