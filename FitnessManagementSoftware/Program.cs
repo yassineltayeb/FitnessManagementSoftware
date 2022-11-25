2d20a81c34388753bbb7b615c23840b5278268e3
yassineltayeb <yassineltayeb@live.com>
2022-11-25 15:44:27 +0400

    enabled cores

--- FitnessManagementSoftware/Program.cs
+++ FitnessManagementSoftware/Program.cs
@@ -70,6 +70,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
