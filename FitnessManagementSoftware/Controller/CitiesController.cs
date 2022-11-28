265ab3dc947111ebd7c90f7bd3dbc6f370b9c5ee
yassineltayeb <yassineltayeb@live.com>
2022-11-29 00:54:45 +0400

    added code to test the pipeline deploy

--- FitnessManagementSoftware/Controller/CitiesController.cs
+++ FitnessManagementSoftware/Controller/CitiesController.cs
@@ -11,7 +11,7 @@ public class CitiesController : ControllerBase
     private readonly ICityService _cityService;
 
     public CitiesController(ICityService cityService)
-	{
+    {
         _cityService = cityService;
     }
 
@@ -19,5 +19,27 @@ public class CitiesController : ControllerBase
     public async Task<List<KeyValuePairs>> GetCitiesByCountryId(int countryId)
     {
         return await _cityService.GetCitiesByCountryId(countryId);
+    }
+
+    [HttpGet("test")]
+    public async Task<List<KeyValuePairs>> GetCities()
+    {
+        var cities = new List<KeyValuePairs>() {
+        new KeyValuePairs()
+            {
+                Id = 1,
+                Name = "City 1"
+            },
+            new KeyValuePairs()
+            {
+                Id = 2,
+                Name = "City 2"
+            },
+            new KeyValuePairs()
+            {
+                Id = 3,
+                Name = "City 3"
+            }};
+        return cities;
     }
 }
