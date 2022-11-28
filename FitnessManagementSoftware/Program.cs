6e580ec930983b94db3671ef32e9040b40ee81c5
yassineltayeb <yassineltayeb@live.com>
2022-11-29 01:24:04 +0400

    added swagger on deploy

--- FitnessManagementSoftware/Program.cs
+++ FitnessManagementSoftware/Program.cs
@@ -61,12 +61,8 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
