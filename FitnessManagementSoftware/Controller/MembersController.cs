27d3f47fa60a1dd2941b313a4a9053a59259686c
yassineltayeb <yassineltayeb@live.com>
2022-11-28 14:13:28 +0400

    added members controller

---
+++ FitnessManagementSoftware/Controller/MembersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interface;
+using Service.ViewModels.Coach;
+
+namespace FitnessManagementSoftware.Controller;
+
+[Route("api/members")]
+[Authorize]
+[ApiController]
+public class MembersController : ControllerBase
+{
+    private readonly IMemberService _memberService;
+
+    public MembersController(IMemberService memberService)
+    {
+        _memberService = memberService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("signup")]
+    public async Task<SignUpResponseViewModel> MemberSignUp([FromBody] SignUpRequestViewModel signUpRequestViewModel)
+    {
+        var signUpResponseViewModel = await _memberService.SignUp(signUpRequestViewModel);
+
+        return signUpResponseViewModel;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<SignUpResponseViewModel> MemberLogin([FromBody] LoginRequestViewModel loginRequestViewModel)
+    {
+        var signUpResponseViewModel = await _memberService.Login(loginRequestViewModel);
+
+        return signUpResponseViewModel;
+    }
+}
