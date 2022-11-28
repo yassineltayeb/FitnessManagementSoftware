69951399bbd0e4a97af6ee7598d775e9ab7d0bee
yassineltayeb <yassineltayeb@live.com>
2022-11-28 13:49:04 +0400

    added member repository

---
+++ Repository/Implementation/MemberRepository.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+
+namespace Repository.Implementation;
+
+public class MemberRepository : IMemberRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public MemberRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Member> AddMember(Member member)
+    {
+        await _dbContext.Members.AddAsync(member);
+        await _dbContext.SaveChangesAsync();
+
+        return member;
+    }
+
+    public async Task<Member> GetMemberhByEmail(string email)
+    {
+        return await _dbContext.Members.SingleOrDefaultAsync(c => c.Email == email);
+    }
+
+    public async Task<bool> VerifyEmail(string email)
+    {
+        return await _dbContext.Members.AnyAsync(c => c.Email == email);
+    }
+}
---
+++ Repository/Interface/IMemberRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Repository.Interface
+{
+    public interface IMemberRepository
+    {
+        Task<Member> AddMember(Member member);
+        Task<Member> GetMemberhByEmail(string email);
+        Task<bool> VerifyEmail(string email);
+    }
+}
