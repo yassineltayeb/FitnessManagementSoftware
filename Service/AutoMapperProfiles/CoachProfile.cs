8c543ce6824b707299a35afc5c9bbd246e1eab20
yassineltayeb <yassineltayeb@live.com>
2022-12-04 16:44:55 +0400

    fixed mapping issue

--- Service/AutoMapperProfiles/CoachProfile.cs
+++ Service/AutoMapperProfiles/CoachProfile.cs
@@ -30,12 +30,16 @@ public class CoachProfile : Profile
             .ForMember(desc => desc.DateOfBirth, o => o.MapFrom(src => src.DateOfBirth))
             .ReverseMap();
 
+        CreateMap<CoachesTypes, int>()
+            .ConvertUsing(src => src.CoachTypeId);
+
         CreateMap<UpdateCoachResponseViewModel, Coach>()
             .ForMember(desc => desc.FirstName, o => o.MapFrom(src => src.FirstName))
             .ForMember(desc => desc.LastName, o => o.MapFrom(src => src.LastName))
             .ForMember(desc => desc.GenderId, o => o.MapFrom(src => src.GenderId))
             .ForMember(desc => desc.Email, o => o.MapFrom(src => src.Email))
             .ForMember(desc => desc.Phone, o => o.MapFrom(src => src.Phone))
+            .ForMember(desc => desc.CoachesTypes, o => o.MapFrom(src => src.CoachTypesIds))
             .ForMember(desc => desc.CountryId, o => o.MapFrom(src => src.CountryId))
             .ForMember(desc => desc.CityId, o => o.MapFrom(src => src.CityId))
             .ForMember(desc => desc.DateOfBirth, o => o.MapFrom(src => src.DateOfBirth))
