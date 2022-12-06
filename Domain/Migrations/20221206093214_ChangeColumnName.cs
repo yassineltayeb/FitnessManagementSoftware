b7a1035027cb7daf8bf6d1495c289c32bf7845a9
yassineltayeb <yassineltayeb@live.com>
2022-12-07 02:04:12 +0400

    renamed comun name

---
+++ Domain/Migrations/20221206093214_ChangeColumnName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                                name: "AvailbleSpaces",
+                                table: "CoachClasses",
+                                newName: "AvailableSpaces");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                                name: "AvailableSpaces",
+                                table: "CoachClasses",
+                                newName: "AvailbleSpaces");
+        }
+    }
+}
