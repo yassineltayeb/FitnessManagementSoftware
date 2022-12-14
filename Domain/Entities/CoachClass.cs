8ecf2eb030e58844bc79f82507ce7d3356b5dd25
yassineltayeb <yassineltayeb@live.com>
2022-12-15 01:30:40 +0400

    added status id to coach  class

--- Domain/Entities/CoachClass.cs
+++ Domain/Entities/CoachClass.cs
@@ -3,10 +3,11 @@ public class CoachClass
 {
     public long Id { get; set; }
     public long CoachId { get; set; }
-    public Coach Coach { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-    public string Location { get; set; }
+    public Coach? Coach { get; set; }
+    public int StatusId { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
     public DateTime ClassFrom { get; set; }
     public DateTime ClassTo { get; set; }
     public int AvailableSpaces { get; set; }
---
+++ Domain/Migrations/20221214213001_AddStatusIdColumnToCoachClassTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusIdColumnToCoachClassTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "CoachClasses",
+                type: "int",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "CoachClasses");
+        }
+    }
+}
