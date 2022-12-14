81c2e8c2877ffa781ee228ff0308af0eb581ba75
yassineltayeb <yassineltayeb@live.com>
2022-12-14 19:31:56 +0400

    added enum extension method

---
+++ Repository/Enum/CoachClassTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Repository.Enum;
+
+public enum CoachClassTypeEnum
+{
+    [Description("Booking")] Booking = 1,
+    [Description("Fully Booked")] FullyBooked = 2,
+    [Description("On Progress")] OnProgress = 3,
+    [Description("Finished")] Finished = 4,
+    [Description("Expired")] Expired = 5,
+    [Description("Canceled")] Canceled = 6,
+}
---
+++ Repository/Extensions/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+namespace Repository.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetEnumDescription(this System.Enum enumValue)
+    {
+        var field = enumValue.GetType().GetField(enumValue.ToString());
+        if (Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) is DescriptionAttribute attribute)
+        {
+            return attribute.Description;
+        }
+        throw new ArgumentException("Item not found.", nameof(enumValue));
+    }
+}
