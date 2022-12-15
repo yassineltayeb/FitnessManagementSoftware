082b400af47ef5f5afff103658bfd45eefcbedcd
yassineltayeb <yassineltayeb@live.com>
2022-12-15 19:29:45 +0400

    removed extra comma

--- Repository/Enum/CoachClassStatusEnum.cs
+++ Repository/Enum/CoachClassStatusEnum.cs
@@ -7,5 +7,5 @@ public enum CoachClassStatusEnum
     [Description("Booking")] Booking = 1,
     [Description("On Progress")] OnProgress = 2,
     [Description("Finished")] Finished = 3,
-    [Description("Canceled")] Canceled = 4,
+    [Description("Canceled")] Canceled = 4
 }
